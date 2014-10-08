f3acb352e0fe54ca3fb6a78646c4a855ad87aa3d
Guus van Weelden <guus.weelden@googlemail.com>
2014-10-08 08:18:31 +0000

    working on leafs

--- leaf.go
+++ leaf.go
@@ -1,8 +1,21 @@
 package main
 
 type Leaf struct {
+	Key   string
+	Value string
 }
 
 func (l Leaf) Eval(attr *Attributes) bool {
-	return true
+	if val, ok := attr.Values[l.Key]; ok {
+		for _, v := range val {
+			if v == l.Value {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (l Leaf) Print() string {
+	return ""
 }
