fc4d62b08e9bbb12e6a87ef18715dc3fb1f092ce
Guus van Weelden <guus.weelden@googlemail.com>
2014-10-08 08:28:40 +0000

    delete contant interface, add Print() to True and False

--- constant.go
+++ constant.go
@@ -1,9 +1,5 @@
 package main
 
-type Constant interface {
-	Eval() bool
-}
-
 type True struct {
 }
 
@@ -11,9 +7,17 @@ func (t True) Eval(attr *Attributes) bool {
 	return true
 }
 
+func (t True) Print() string {
+	return "TRUE"
+}
+
 type False struct {
 }
 
 func (f False) Eval(attr *Attributes) bool {
 	return false
+}
+
+func (f False) Print() string {
+	return "FALSE"
 }
